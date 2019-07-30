bc07eaea94c8e6196786e1d4251001e50f318245
Henry Chung <chungthehao@gmail.com>
2019-07-30 14:44:09 +0700

    Eloquent, Custom accessors

--- app/Team.php
+++ app/Team.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Team extends Model
 {
-    //
+    public $appends = ['users_count']; // Lay accessor nao ra luon.
+
+    // Accessor
+    public function getUsersCountAttribute()
+    {
+        return \DB::table('users')->where('users.team_id', $this->id)->count();
+    }
 }

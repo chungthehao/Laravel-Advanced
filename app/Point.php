fddf9977b8db62f3ec660c62034749b263b115a6
Henry Chung <chungthehao@gmail.com>
2019-07-30 13:38:11 +0700

    Eloquent, Global scoping

--- app/Point.php
+++ app/Point.php
@@ -2,9 +2,16 @@
 
 namespace App;
 
+use App\Scopes\PointScope;
 use Illuminate\Database\Eloquent\Model;
 
 class Point extends Model
 {
-    //
+    protected static function boot()
+    {
+        parent::boot();
+
+        // This adds our scope onto the model for all queries performed.
+        static::addGlobalScope(new PointScope());
+    }
 }

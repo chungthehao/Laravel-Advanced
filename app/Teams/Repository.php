6e7d6151f724dd7a8386be4540b307d77047a5ad
Henry Chung <chungthehao@gmail.com>
2019-07-30 15:30:18 +0700

    Eloquent, Advanced wheres

--- app/Teams/Repository.php
+++ app/Teams/Repository.php
@@ -6,9 +6,15 @@ class Repository {
 
     public function points($team)
     {
+        // Sub query, de xai cho whereIn o duoi
+        $userIds = $team->where('teams.id', $team->id)
+            ->join('users', 'users.team_id', '=', 'teams.id')
+            ->select('users.id');
+
         return $team->where('teams.id', $team->id)
             ->join('tickets', 'tickets.team_id', '=', 'teams.id')
             ->join('points', 'points.ticket_id', '=', 'tickets.id')
+            ->whereIn('points.owner_id', $userIds)
             ->sum('points.value');
     }
 

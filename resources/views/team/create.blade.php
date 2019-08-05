289b057fa7f8ac18c88e66ef6f232400700f9301
Henry Chung <chungthehao@gmail.com>
2019-08-05 09:23:08 +0700

    Custom blade directives

--- resources/views/team/create.blade.php
+++ resources/views/team/create.blade.php
@@ -3,10 +3,7 @@
 @section('content')
     <form action="{{ action('Web\TeamController@store') }}" method="post">
         @csrf
-        <div class="form-group">
-            <label for="title">Title</label>
-            <input type="text" class="form-control" name="title" id="title">
-        </div>
+        @inputtextbox('title')
         <button type="submit" class="btn btn-primary">Create</button>
     </form>
 @endsection
